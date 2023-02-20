be94019bbb1772091b7f8603fe2b0b5a5069339a
Char <56329591+pa21paris@users.noreply.github.com>
2023-02-19 23:15:59 -0300

    started coding

---
+++ code.js
@@ -0,0 +1,12 @@
+const square=document.querySelector('.square');
+const circle=document.querySelector('.circle');
+
+function sleep(sleepTime){
+    return new Promise((resolve)=>{
+        setTimeout(resolve(),sleepTime);
+    })
+}
+
+function changeDIVWidth(minWidth,maxWidth){
+    
+}
